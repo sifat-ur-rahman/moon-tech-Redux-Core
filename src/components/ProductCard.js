c4b128624934b164ce6ceb269edada849e968bda
sifat <md.sifat.ur.rahman2702@gmail.com>
2023-03-09 09:51:20 +0600

    add3

--- src/components/ProductCard.js
+++ src/components/ProductCard.js
@@ -1,9 +1,12 @@
 import React from "react";
 import { BiListPlus } from "react-icons/bi";
+import { useDispatch } from "react-redux";
+import { addToCart } from "../redux/actionCretors/productActions";
+import { ADD_TO_CART } from "../redux/actionType/actionType";
 
 
 const ProductCard = ({ product }) => {
-  
+  const dispatch = useDispatch()
 
   return (
     <div
@@ -25,7 +28,7 @@ const ProductCard = ({ product }) => {
       <div className='flex gap-2 mt-5'>
         <button
           className='bg-indigo-500 rounded-full py-1 px-2 flex-1 text-white text-bold'
-          
+          onClick={()=> dispatch(addToCart(product))}
         >
           Add to cart
         </button>
--- src/redux/reducers/productReducer.js
+++ src/redux/reducers/productReducer.js
@@ -1,9 +1,22 @@
+import { ADD_TO_CART, REMOVE_FROM_CART } from "../actionType/actionType";
+
 const initialState = {
-    test: "test"
+   cart: [],
 };
 
 const productReducer = (state = initialState, action) =>{
-    return state;
+   switch(action.type){
+    case ADD_TO_CART:
+        return{
+            ...state,
+            cart: [...state.cart, action.payload]
+        };
+    case REMOVE_FROM_CART:
+        return{};
+    default:
+        return state
+   }
+  
 }
 
 export default productReducer;
