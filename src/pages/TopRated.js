ffb2ee4d3f4c2b8ebb1d2559f855556f64e93e18
sifat <md.sifat.ur.rahman2702@gmail.com>
2023-03-09 09:00:17 +0600

    first commit

---
+++ src/pages/TopRated.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+
+const TopRated = () => {
+  
+  return (
+    <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 max-w-7xl gap-14 mx-auto my-10'>
+     TopRated
+    </div>
+  );
+};
+
+export default TopRated;
